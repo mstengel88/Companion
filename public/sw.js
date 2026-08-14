4b21897cfdfb32c85a8c884e503013f3d288d270
Matthew Stengel <matthewstengel69@gmail.com>
2026-08-13 23:05:59 -0500

    fix Safari chat submission

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "emily-shell-v5.6.3";
+const CACHE = "emily-shell-v5.6.4";
 const SHELL = ["/", "/styles.css", "/app.js", "/manifest.webmanifest", "/emily-icon.svg"];
 
 self.addEventListener("install", (event) => {
