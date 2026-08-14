59b0be2c14cb0b17e01ec03d29ed807fee72b658
Matthew Stengel <matthewstengel69@gmail.com>
2026-08-13 22:08:27 -0500

    Improve chat send and scrolling

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "emily-shell-v5.6.1";
+const CACHE = "emily-shell-v5.6.2";
 const SHELL = ["/", "/styles.css", "/app.js", "/manifest.webmanifest", "/emily-icon.svg"];
 
 self.addEventListener("install", (event) => {
